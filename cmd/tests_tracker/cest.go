7a763290116f57e31f46227d1f43bf2810b8f1c5
Redjol Resulaj <redjol.resulaj@yahoo.com>
2024-05-24 11:17:45 +0200

    Storing events to Cest

--- cmd/tests_tracker/cest.go
+++ cmd/tests_tracker/cest.go
@@ -2,7 +2,12 @@ package tests_tracker
 
 import (
 	"github.com/redjolr/goherent/cmd/events"
+	"github.com/redjolr/goherent/cmd/events/test_continued_event"
+	"github.com/redjolr/goherent/cmd/events/test_failed_event"
+	"github.com/redjolr/goherent/cmd/events/test_passed_event"
+	"github.com/redjolr/goherent/cmd/events/test_paused_event"
 	"github.com/redjolr/goherent/cmd/events/test_ran_event"
+	"github.com/redjolr/goherent/cmd/events/test_skipped_event"
 )
 
 type Cest struct {
@@ -23,6 +28,31 @@ func (cest *Cest) IsRunning() bool {
 }
 
 func (cest *Cest) NewRanEvent(evt test_ran_event.TestRanEvent) {
+	cest.isRunning = true
+	cest.events = append(cest.events, evt)
+}
+
+func (cest *Cest) NewPassedEvent(evt test_passed_event.TestPassedEvent) {
+	cest.isRunning = false
+	cest.events = append(cest.events, evt)
+}
+
+func (cest *Cest) NewFailedEvent(evt test_failed_event.TestFailedEvent) {
+	cest.isRunning = false
+	cest.events = append(cest.events, evt)
+}
+
+func (cest *Cest) NewPausedEvent(evt test_paused_event.TestPausedEvent) {
+	cest.isRunning = false
+	cest.events = append(cest.events, evt)
+}
+
+func (cest *Cest) NewSkippedEvent(evt test_skipped_event.TestSkippedEvent) {
+	cest.isRunning = false
+	cest.events = append(cest.events, evt)
+}
+
+func (cest *Cest) NewContinuedEvent(evt test_continued_event.TestContinuedEvent) {
 	cest.isRunning = true
 	cest.events = append(cest.events, evt)
 }
--- cmd/tests_tracker/cest_test.go
+++ cmd/tests_tracker/cest_test.go
@@ -5,7 +5,12 @@ import (
 	"time"
 
 	"github.com/redjolr/goherent/cmd/events"
+	"github.com/redjolr/goherent/cmd/events/test_continued_event"
+	"github.com/redjolr/goherent/cmd/events/test_failed_event"
+	"github.com/redjolr/goherent/cmd/events/test_passed_event"
+	"github.com/redjolr/goherent/cmd/events/test_paused_event"
 	"github.com/redjolr/goherent/cmd/events/test_ran_event"
+	"github.com/redjolr/goherent/cmd/events/test_skipped_event"
 	. "github.com/redjolr/goherent/pkg"
 	"github.com/stretchr/testify/assert"
 )
@@ -25,5 +30,133 @@ func TestCestIsRunning(t *testing.T) {
 			),
 		)
 		assert.True(cest.IsRunning())
+	}, t)
+
+	Test("it should return false, if the Cest instance has a TestRanEvent and a TestPassedEvent", func(t *testing.T) {
+		cest := NewCest("cestName")
+		cest.NewRanEvent(
+			test_ran_event.NewFromJsonTestEvent(
+				events.JsonTestEvent{
+					Time:    time.Now(),
+					Package: "somePackage",
+					Test:    "someTestName",
+				},
+			),
+		)
+		cest.NewPassedEvent(
+			test_passed_event.NewFromJsonTestEvent(
+				events.JsonTestEvent{
+					Time:    time.Now(),
+					Package: "somePackage",
+					Test:    "someTestName",
+					Elapsed: 1.2,
+				},
+			),
+		)
+		assert.False(cest.IsRunning())
+	}, t)
+
+	Test("it should return false, if the Cest instance has a TestRanEvent and a TestFailedEvent", func(t *testing.T) {
+		cest := NewCest("cestName")
+		cest.NewRanEvent(
+			test_ran_event.NewFromJsonTestEvent(
+				events.JsonTestEvent{
+					Time:    time.Now(),
+					Package: "somePackage",
+					Test:    "someTestName",
+				},
+			),
+		)
+		cest.NewFailedEvent(
+			test_failed_event.NewFromJsonTestEvent(
+				events.JsonTestEvent{
+					Time:    time.Now(),
+					Package: "somePackage",
+					Test:    "someTestName",
+					Elapsed: 1.2,
+				},
+			),
+		)
+		assert.False(cest.IsRunning())
+	}, t)
+
+	Test("it should return false, if the Cest instance has a TestRanEvent and a TestPausedEvent", func(t *testing.T) {
+		cest := NewCest("cestName")
+		cest.NewRanEvent(
+			test_ran_event.NewFromJsonTestEvent(
+				events.JsonTestEvent{
+					Time:    time.Now(),
+					Package: "somePackage",
+					Test:    "someTestName",
+				},
+			),
+		)
+		cest.NewPausedEvent(
+			test_paused_event.NewFromJsonTestEvent(
+				events.JsonTestEvent{
+					Time:    time.Now(),
+					Package: "somePackage",
+					Test:    "someTestName",
+				},
+			),
+		)
+		assert.False(cest.IsRunning())
+	}, t)
+
+	Test("it should return true, if the Cest instance has TestRanEvent, TestPausedEvent, and TestContinuedEvent", func(t *testing.T) {
+		cest := NewCest("cestName")
+		cest.NewRanEvent(
+			test_ran_event.NewFromJsonTestEvent(
+				events.JsonTestEvent{
+					Time:    time.Now(),
+					Package: "somePackage",
+					Test:    "someTestName",
+				},
+			),
+		)
+		cest.NewPausedEvent(
+			test_paused_event.NewFromJsonTestEvent(
+				events.JsonTestEvent{
+					Time:    time.Now(),
+					Package: "somePackage",
+					Test:    "someTestName",
+					Elapsed: 1.2,
+				},
+			),
+		)
+		cest.NewContinuedEvent(
+			test_continued_event.NewFromJsonTestEvent(
+				events.JsonTestEvent{
+					Time:    time.Now(),
+					Package: "somePackage",
+					Test:    "someTestName",
+				},
+			),
+		)
+		assert.True(cest.IsRunning())
+	}, t)
+
+	Test("it should return false, if the Cest instance has a TestRanEvent and a TestSkippedEvent", func(t *testing.T) {
+		cest := NewCest("cestName")
+		cest.NewRanEvent(
+			test_ran_event.NewFromJsonTestEvent(
+				events.JsonTestEvent{
+					Time:    time.Now(),
+					Package: "somePackage",
+					Test:    "someTestName",
+				},
+			),
+		)
+		cest.NewSkippedEvent(
+			test_skipped_event.NewFromJsonTestEvent(
+				events.JsonTestEvent{
+					Time:    time.Now(),
+					Package: "somePackage",
+					Test:    "someTestName",
+					Elapsed: 1.2,
+				},
+			),
+		)
+		assert.False(cest.IsRunning())
 	}, t)
 }
