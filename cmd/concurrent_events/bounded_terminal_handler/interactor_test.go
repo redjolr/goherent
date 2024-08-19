d7b8a18ae6df534d329b046b0008e04544be1680
Redjol Resulaj <redjol.resulaj@yahoo.com>
2024-08-19 10:19:59 +0200

    Improving the parallel tests by printing a basic summary in every new package that is started

--- cmd/concurrent_events/bounded_terminal_handler/interactor_test.go
+++ cmd/concurrent_events/bounded_terminal_handler/interactor_test.go
@@ -9,6 +9,7 @@ import (
 	"github.com/redjolr/goherent/cmd/ctests_tracker"
 	"github.com/redjolr/goherent/cmd/events"
 	. "github.com/redjolr/goherent/pkg"
+	"github.com/redjolr/goherent/terminal/ansi_escape"
 	"github.com/redjolr/goherent/terminal/fake_ansi_terminal"
 	"github.com/stretchr/testify/assert"
 )
@@ -21,7 +22,20 @@ func setup(terminalHeight int) (*bounded_terminal_handler.Interactor, *fake_ansi
 	return &interactor, &fakeAnsiTerminal, &ctestTracker
 }
 
-func TestHandlePackageStartedEvent(t *testing.T) {
+func makeNPackageStartedEvents(packageNames ...string) map[string]events.PackageStartedEvent {
+	evts := make(map[string]events.PackageStartedEvent)
+	for _, packName := range packageNames {
+		evts[packName] = events.NewPackageStartedEvent(
+			events.JsonTestEvent{
+				Time:    time.Now(),
+				Action:  "start",
+				Package: packName,
+			})
+	}
+	return evts
+}
+
+func TestHandlePackageStartedEvent_TerminalHeightLessThan5(t *testing.T) {
 	assert := assert.New(t)
 
 	Test(`
@@ -80,31 +94,99 @@ func TestHandlePackageStartedEvent(t *testing.T) {
 	 And we have a bounded terminal with height 1
 	 When a HandlePackageStartedEvent for package "somePackage 2" occurs
 	 And the printed text in the viewport should be "⏳ somePackage 1"`, func(t *testing.T) {
+		packStartedEvents := makeNPackageStartedEvents("somePackage 1", "somePackage 2")
+
 		// Given
 		eventsHandler, terminal, _ := setup(1)
-		packStartedEvt1 := events.NewPackageStartedEvent(
-			events.JsonTestEvent{
-				Time:    time.Now(),
-				Action:  "start",
-				Package: "somePackage 1",
-			},
-		)
-		eventsHandler.HandlePackageStartedEvent(packStartedEvt1)
+		eventsHandler.HandlePackageStartedEvent(packStartedEvents["somePackage 1"])
 
 		// When
-		packStartedEvt2 := events.NewPackageStartedEvent(
-			events.JsonTestEvent{
-				Time:    time.Now(),
-				Action:  "start",
-				Package: "somePackage 2",
-			},
-		)
-		eventsHandler.HandlePackageStartedEvent(packStartedEvt2)
+		eventsHandler.HandlePackageStartedEvent(packStartedEvents["somePackage 2"])
 
 		// Then
 		assert.Equal(
 			terminal.Text(),
 			"⏳ somePackage 1",
+		)
+	}, t)
+
+	Test(`
+	 Given that no events have occurred
+	 And we have a bounded terminal with height 4
+	 When 3 HandlePackageStartedEvent for packages "package 1", "package 2", "package 3", "package 4" occur
+	 And the printed text should be "⏳ package 1\n⏳ package 2\n⏳ package 3\n⏳ package 4"`, func(t *testing.T) {
+		packStartedEvents := makeNPackageStartedEvents("package 1", "package 2", "package 3", "package 4")
+
+		// Given
+		eventsHandler, terminal, _ := setup(4)
+
+		// When
+		eventsHandler.HandlePackageStartedEvent(packStartedEvents["package 1"])
+		eventsHandler.HandlePackageStartedEvent(packStartedEvents["package 2"])
+		eventsHandler.HandlePackageStartedEvent(packStartedEvents["package 3"])
+		eventsHandler.HandlePackageStartedEvent(packStartedEvents["package 4"])
+
+		// Then
+		assert.Equal(
+			terminal.Text(),
+			"⏳ package 1\n⏳ package 2\n⏳ package 3\n⏳ package 4",
+		)
+	}, t)
+
+	Test(`
+	Given that no events have occurred
+	And we have a bounded terminal with height 4
+	When 3 HandlePackageStartedEvent for packages "package 1", "package 2", "package 3", "package 4", "package 5" occur
+	And the printed text should be "⏳ package 1\n⏳ package 2\n⏳ package 3\n⏳ package 4"`, func(t *testing.T) {
+		packStartedEvents := makeNPackageStartedEvents("package 1", "package 2", "package 3", "package 4", "package 5")
+
+		// Given
+		eventsHandler, terminal, _ := setup(4)
+
+		// When
+		eventsHandler.HandlePackageStartedEvent(packStartedEvents["package 1"])
+		eventsHandler.HandlePackageStartedEvent(packStartedEvents["package 2"])
+		eventsHandler.HandlePackageStartedEvent(packStartedEvents["package 3"])
+		eventsHandler.HandlePackageStartedEvent(packStartedEvents["package 4"])
+		eventsHandler.HandlePackageStartedEvent(packStartedEvents["package 5"])
+
+		// Then
+		assert.Equal(
+			terminal.Text(),
+			"⏳ package 1\n⏳ package 2\n⏳ package 3\n⏳ package 4",
+		)
+	}, t)
+}
+
+func TestHandlePackageStartedEvent_TerminalHeightGreaterThan4(t *testing.T) {
+	assert := assert.New(t)
+
+	Test(`
+	Given that no events have occurred
+	And we have a bounded terminal with height 5
+	When 3 HandlePackageStartedEvent for packages "package 1" occur
+	And the printed text should be "⏳ package 1" and the summary of tests:
+	"<bold>Packages</bold>: 1 running\n<bold>Tests</bold>: 0 running\n<bold>Time</bold>: 0.000s"`, func(t *testing.T) {
+		// Given
+		eventsHandler, terminal, _ := setup(5)
+
+		// When
+		packStartedEvt1 := events.NewPackageStartedEvent(
+			events.JsonTestEvent{
+				Time:    time.Now(),
+				Action:  "start",
+				Package: "package 1",
+			},
+		)
+		eventsHandler.HandlePackageStartedEvent(packStartedEvt1)
+
+		// Then
+		assert.Equal(
+			terminal.Text(),
+			"⏳ package 1"+
+				"\n"+ansi_escape.BOLD+"Packages:"+ansi_escape.RESET_BOLD+" 1 running"+
+				"\n"+ansi_escape.BOLD+"Tests:"+ansi_escape.RESET_BOLD+"    0 running"+
+				"\n"+ansi_escape.BOLD+"Time:"+ansi_escape.RESET_BOLD+"     0.000s",
 		)
 	}, t)
 }
--- cmd/concurrent_events/bounded_terminal_handler/presenter.go
+++ cmd/concurrent_events/bounded_terminal_handler/presenter.go
@@ -1,6 +1,8 @@
 package bounded_terminal_handler
 
 import (
+	"fmt"
+
 	"github.com/redjolr/goherent/cmd/ctests_tracker"
 	"github.com/redjolr/goherent/terminal"
 	"github.com/redjolr/goherent/terminal/ansi_escape"
@@ -17,13 +19,25 @@ func NewPresenter(term terminal.Terminal) Presenter {
 }
 
 func (p *Presenter) DisplayPackages(packagesUt []*ctests_tracker.PackageUnderTest) {
-	packagesThatFitInTerminal := packagesUt[0:p.terminal.Height()]
+	packagesSummary := ansi_escape.BOLD + "Packages:" + ansi_escape.RESET_BOLD + " "
+	testsSummary := ansi_escape.BOLD + "Tests:" + ansi_escape.RESET_BOLD + "    "
+	timeSummary := ansi_escape.BOLD + "Time:" + ansi_escape.RESET_BOLD + "     0.000s"
+	packagesThatFitInTerminal := packagesUt[0:min(len(packagesUt), p.terminal.Height())]
 	for i, packageut := range packagesThatFitInTerminal {
 		if i != 0 {
 			p.terminal.Print("\n")
 		}
 		p.terminal.Print("⏳ " + packageut.Name())
 	}
+
+	p.terminal.Print(
+		fmt.Sprintf(
+			"\n" +
+				packagesSummary + "1 running\n" +
+				testsSummary + "0 running\n" +
+				timeSummary,
+		),
+	)
 }
 
 func (p *Presenter) EraseScreen() {
