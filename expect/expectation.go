fc1cfc1e76a4e03b4d2dc087574d61fd564b9ec8
Redjol Resulaj <redjol.resulaj@yahoo.com>
2024-09-12 10:06:25 +0200

    Printing the file where the assertion failed

--- expect/expectation.go
+++ expect/expectation.go
@@ -1,9 +1,14 @@
 package expect
 
 import (
+	"fmt"
+	"runtime"
+	"strings"
 	"testing"
 
 	"github.com/redjolr/goherent/expect/internal/assertions"
+	"github.com/redjolr/goherent/internal/utils"
+	"github.com/redjolr/goherent/terminal/ansi_escape"
 )
 
 type expectation struct {
@@ -13,54 +18,88 @@ type expectation struct {
 
 func (e *expectation) ToEqual(actual any) {
 	if err := assertions.ToEqual(e.checkExpectationAgainst, actual); err != nil {
-		e.t.Errorf(err.Error())
+		_, file, line, _ := runtime.Caller(1)
+		e.print(fmt.Sprintf(ansi_escape.YELLOW+"%s:%d"+ansi_escape.COLOR_RESET, file, line), 4)
+		e.print(err.Error(), 6)
+		e.t.Fail()
 	}
 }
 
 func (e *expectation) ToContain(containee any) {
 	if err := assertions.ToContain(e.checkExpectationAgainst, containee); err != nil {
-		e.t.Errorf(err.Error())
+		_, file, line, _ := runtime.Caller(1)
+		e.print(fmt.Sprintf(ansi_escape.YELLOW+"%s:%d"+ansi_escape.COLOR_RESET, file, line), 4)
+		e.print(err.Error(), 6)
+		e.t.Fail()
 	}
 }
 
 func (e *expectation) ToBeError() {
 	if err := assertions.ToBeError(e.checkExpectationAgainst); err != nil {
-		e.t.Errorf(err.Error())
+		_, file, line, _ := runtime.Caller(1)
+		e.print(fmt.Sprintf(ansi_escape.YELLOW+"%s:%d"+ansi_escape.COLOR_RESET, file, line), 4)
+		e.print(err.Error(), 6)
+		e.t.Fail()
 	}
 }
 
 func (e *expectation) NotToBeError() {
 	if err := assertions.NotToBeError(e.checkExpectationAgainst); err != nil {
-		e.t.Errorf(err.Error())
+		_, file, line, _ := runtime.Caller(1)
+		e.print(fmt.Sprintf(ansi_escape.YELLOW+"%s:%d"+ansi_escape.COLOR_RESET, file, line), 4)
+		e.print(err.Error(), 6)
+		e.t.Fail()
 	}
 }
 
 func (e *expectation) ToBeTrue() {
 	if err := assertions.ToBeTrue(e.checkExpectationAgainst); err != nil {
-		e.t.Errorf(err.Error())
+		_, file, line, _ := runtime.Caller(1)
+		e.print(fmt.Sprintf(ansi_escape.YELLOW+"%s:%d"+ansi_escape.COLOR_RESET, file, line), 4)
+		e.print(err.Error(), 6)
+		e.t.Fail()
 	}
 }
 
 func (e *expectation) ToBeFalse() {
 	if err := assertions.ToBeFalse(e.checkExpectationAgainst); err != nil {
-		e.t.Errorf(err.Error())
+		_, file, line, _ := runtime.Caller(1)
+		e.print(fmt.Sprintf(ansi_escape.YELLOW+"%s:%d"+ansi_escape.COLOR_RESET, file, line), 4)
+		e.print(err.Error(), 6)
+		e.t.Fail()
 	}
 }
 
 func (e *expectation) ToBeNil() {
 	if err := assertions.ToBeNil(e.checkExpectationAgainst); err != nil {
-		e.t.Errorf(err.Error())
+		_, file, line, _ := runtime.Caller(1)
+		e.print(fmt.Sprintf(ansi_escape.YELLOW+"%s:%d"+ansi_escape.COLOR_RESET, file, line), 4)
+		e.print(err.Error(), 6)
+		e.t.Fail()
 	}
 }
 
 func (e *expectation) NotToBeNil() {
 	if err := assertions.NotToBeNil(e.checkExpectationAgainst); err != nil {
-		e.t.Errorf(err.Error())
+		_, file, line, _ := runtime.Caller(1)
+		e.print(fmt.Sprintf(ansi_escape.YELLOW+"%s:%d"+ansi_escape.COLOR_RESET, file, line), 4)
+		e.print(err.Error(), 6)
+		e.t.Fail()
 	}
 }
 
 func (e *expectation) ToBeOfSameTypeAs(compareVal any) {
 	if err := assertions.ToBeOfSameTypeAs(e.checkExpectationAgainst, compareVal); err != nil {
-		e.t.Errorf(err.Error())
+		_, file, line, _ := runtime.Caller(1)
+		e.print(fmt.Sprintf(ansi_escape.YELLOW+"%s:%d"+ansi_escape.COLOR_RESET, file, line), 4)
+		e.print(err.Error(), 6)
+		e.t.Fail()
+	}
+}
+
+func (e *expectation) print(str string, leftPadWhitespace int) {
+	lines := utils.SplitStringByNewLine(str)
+	for _, line := range lines {
+		fmt.Println(strings.Repeat(" ", leftPadWhitespace) + line)
 	}
 }
