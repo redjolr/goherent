17645e6d1b19edebe4895c3d8b61b637661a3374
Redjol Resulaj <redjol.resulaj@yahoo.com>
2025-01-07 17:38:53 +0000

    Fixed a bug with ToBeGreaterThan, where the position of the compared elements was the opposite of the one that should have been

--- expect/expectation.go
+++ expect/expectation.go
@@ -113,8 +113,8 @@ func (e *expectation) ToBeString() {
 	}
 }
 
-func (e *expectation) ToBeGreaterThan(actual any) {
-	if err := assertions.ToBeGreaterThan(actual, e.checkExpectationAgainst); err != nil {
+func (e *expectation) ToBeGreaterThan(checkIfGreaterAgainst any) {
+	if err := assertions.ToBeGreaterThan(e.checkExpectationAgainst, checkIfGreaterAgainst); err != nil {
 		_, file, line, _ := runtime.Caller(1)
 		e.print(fmt.Sprintf(ansi_escape.YELLOW+"%s:%d"+ansi_escape.COLOR_RESET, file, line), 4)
 		e.print(err.Error(), 6)
