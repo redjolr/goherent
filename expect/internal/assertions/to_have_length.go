28020c6e5acc83061d7025a1911838bbaa8bca85
Redjol Resulaj <redjol.resulaj@volksai.io>
2025-05-17 14:33:20 +0000

    Fixed some error messages

--- expect/internal/assertions/to_have_length.go
+++ expect/internal/assertions/to_have_length.go
@@ -6,19 +6,19 @@ import (
 	"reflect"
 )
 
-func ToHaveLength(v any, length int) error {
+func ToHaveLength(v any, expectedLength int) error {
 	if v == nil {
 		return errors.New("a nil value cannot be checked if it has length, but it is")
 	}
 	switch reflect.TypeOf(v).Kind() {
 	case reflect.Array, reflect.Chan, reflect.Map, reflect.Slice, reflect.String:
-		valueLength := reflect.ValueOf(v).Len()
-		if valueLength == length {
+		actualLength := reflect.ValueOf(v).Len()
+		if actualLength == expectedLength {
 			return nil
 		}
-		return fmt.Errorf("value %v does not have length %d", v, length)
+		return fmt.Errorf("expected lenght: %d. actual length: %d ", expectedLength, actualLength)
 	default:
 		typeName := reflect.TypeOf(v).Name()
-		return fmt.Errorf("type %v of value %v does not have a length", typeName, v)
+		return fmt.Errorf("type %v cannot be passed to the len() function, so it does not have a length", typeName)
 	}
 }
